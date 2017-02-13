7462c05ea27dcfe72974734bb352a7140d0227f2
hyporational <jasontaylorwalters@gmail.com>
2017-02-13 14:28:37 -0600

    Add files via upload

---
+++ ProgrammingAssingment2.R
@@ -0,0 +1,34 @@
+## This is the Programming assignment for Week 3, inverting matrices and cacheing the results using lexical scoping 
+
+
+## makeCacheMatrix creates an R object that stores a matrix and the matrix's Inverse 
+
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}
+
+
+## cacheSolve takes an argument from makeCacheMatrix and retrieves the Inverse 
+## from the cached value stored in the makeCacheMatrix environment
+
+cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
